ea0b7c857099f285dc6c60c8724e7bd6b0ed44f6
Michael Heimes <michael@cairndigitalmedia.com>
2024-10-07 11:07:32 -0400

    dev

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -10,15 +10,20 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		
-	<div class="entry-content">
-        <div class="grid-container">
-            <div class="grid-x grid-padding-x">
-                <div class="cell small-12">
+	<img src="<?php echo get_template_directory_uri(); ?>/assets/images/alza-interior-topper.webp">
+	<section class="entry-content">
+        <div class="grid-container relative">
+            <div class="grid-x grid-padding-x align-center">
+                <div class="cell small-12 large-10 xlarge-8">
+                    <?php the_content();?>
 		            <?php get_template_part('template-parts/loop-modules');?>
                 </div>
             </div>
         </div>
-	</div><!-- .entry-content -->
+	</section><!-- .entry-content -->
+        
+<footer class="article-footer">
+        <?php get_template_part('template-parts/section', 'footer-cta');?>
+</footer> <!-- end article footer -->
 
 </article><!-- #post-<?php the_ID(); ?> -->
